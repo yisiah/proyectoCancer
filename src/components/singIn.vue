9bb6f509adc2e22952ea379745feabc4cf07b85f
yisiah <harita.1818@gmail.com>
2020-07-06 21:04:45 -0500

    first commit

---
+++ src/components/singIn.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div>
+    <h1>{{titulo}}</h1>
+    <div>
+      <b-button>Button</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+        titulo: "Hola"
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
